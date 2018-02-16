7772b225c50e4819ce74a1e6039926ab726eaa09
daw2 <daw2@daw2m08.fjeclot.net>
2018-02-16 20:24:43 +0100

    Commit 1 del projecte ascii2

---
+++ ascii2.c
@@ -0,0 +1,16 @@
+//Autor: Daniel Ubeda Sanchez
+#include <stdio.h>
+
+int main() {
+	char c;
+	int n,i;
+		
+	printf("TAULA ASCII MAJÚSCULES 14-2-2018\n");
+	printf("CAR  HEX  DEC\n");
+	n='Z'-'A';
+	for (i=n;i>=0;i--) {
+		c='A'+i;
+		printf("%c    %x   %d\n",c,c,c);
+	}
+	return(0);
+}
